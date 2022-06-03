0ca0957a974dd40df3651505f66169ca964535d6
KelvynLenis <kmartinslenis30@hotmail.com>
2022-06-03 14:47:17 -0300

    adicionando o algoritmo guloso

--- main.cpp
+++ main.cpp
@@ -7,6 +7,36 @@
 
 using namespace std;
 
+void print2DVector(vector<vector<int>> vec){
+  for ( int i = 0; i < vec.size(); i++ ){
+    for ( int j = 0; j < vec[i].size(); j++) {
+      cout << vec[i][j] << "; ";
+    }
+  }
+  cout << endl;
+}
+
+void showAllocationLogs(vector<vector<int>> servers, vector<int> b, int totalCost){
+  // cout << "Servers running: " << servers.size();
+  // cout << endl;
+
+  // cout << "Allocated jobs on server 1: " << servers[0].size();
+  // cout << endl;
+  for ( int i = 0; i < servers.size(); i++ ){
+    int tempTime = 0;
+    for ( int j = 0; j < servers[i].size(); j++) {
+      cout << servers[i][j] << ", ";
+      tempTime += servers[i][j];
+    }
+    // cout << endl;
+    cout << "Server " << i << " capacity is " << b[i] << endl;
+    cout << "Total time allocated is " << tempTime;
+    cout << endl;
+  }
+
+  cout << "Solution cost: " << totalCost << endl;
+}
+
 int main(int argc, char *argv[]){
 
   // ========== definição de variáveis ===================
@@ -14,6 +44,8 @@ int main(int argc, char *argv[]){
   vector<int> b; // vetor de capacidade
   vector<vector<int>> t; // vetor de tempo
   vector<vector<int>> c; // vetor de custo
+  vector<vector<int>> servers; // vetor de servidores
+  int numberOfServers, numberOfJobs, penalty = 0;
 
   // ============== Input do arquivo =====================
 
@@ -23,7 +55,6 @@ int main(int argc, char *argv[]){
   int num;
   int temp = 0;
 
-  int numberOfServers, numberOfJobs, penalidade = 0;
   int lineCounter = 1;
 
   if(myfile.is_open()) {
@@ -32,15 +63,15 @@ int main(int argc, char *argv[]){
 
       if(lineCounter == 1){ // primeira linha
         numberOfJobs = stoi(line); // numero de jobs
-        cout << numberOfJobs << endl;
+        // cout << numberOfJobs << endl;
       } 
       else if(lineCounter == 2){ // segunda linha
         numberOfServers = stoi(line); // numero de servidores
-        cout << numberOfServers << endl;
+        // cout << numberOfServers << endl;
       }
       else if(lineCounter == 3){ // terceira linha
-        penalidade = stoi(line); // penalidade de cada penalidade
-        cout << penalidade << endl;
+        penalty = stoi(line); // penalty de cada penalty
+        // cout << penalty << endl;
       }
       
       if(lineCounter == 5) { // quinta linha
@@ -80,31 +111,40 @@ int main(int argc, char *argv[]){
     cout << "Unable to open file";
   }
 
-  cout << "T size is: ";
-  cout << t.size() << " x ";
-  cout << t[0].size() << endl;
+  // cout << "T size is: ";
+  // cout << t.size() << " x ";
+  // cout << t[0].size() << endl;
   
-  cout << "C size is: ";
-  cout << c.size() << " x ";
-  cout << c[0].size() << endl;
+  // cout << "C size is: ";
+  // cout << c.size() << " x ";
+  // cout << c[0].size() << endl;
 
-  // ======================================
+  // ========================================================
+  // ===== heuristica de construção (algoritmo guloso) ======
+  // ========================================================
 
+  // Implementação simples de um algoritmo guloso longe do ideal
 
-  // heuristica de construção (algoritmo guloso)
-  // to do
+  int totalCost = 0; // custo da solução
 
-  // for ( int i = 0; i < t.size(); i++ ){
-  //   for ( int j = 0; j < t[i].size(); j++) {
-  //     cout << t[i][j] << "; ";
-  //   }
-  // }
+  // criando os servidores e alocando tantos jobs quanto possivel
+  for(int i = 0; i < numberOfServers; i++){
+    servers.push_back(vector<int>());
+    int allocatedTime = 0;
+    for(int j = 0; j < numberOfJobs; j++){
+      if(t[i][j] + allocatedTime <= b[i]){
+        servers[i].push_back(t[i][j]);
+        allocatedTime += t[i][j];
+        totalCost += c[i][j];
+      }
+      else {
+        totalCost += penalty;
+      }
+    }
+  }
 
-  // for ( int i = 0; i < c.size(); i++ ){
-  //   for ( int j = 0; j < c[i].size(); j++) {
-  //     cout << c[i][j] << "; ";
-  //   }
-  // }
+  // showAllocationLogs(servers, b, totalCost);
+  // print2DVector(t);
 
   // vizinhaça
   // to do
